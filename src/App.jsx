edda55e4b6ba8bd85f9b0887c880c3255cf60ffb
MARK RUBANZA <markrubanza10@gmail.com>
2023-06-07 17:38:54 +0300

    make some changes about the logo

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,23 @@ import Resume from './components/Resume';
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container">
-            <a className="navbar-brand" href="#">Portfolio</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#about">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#projects">Projects</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#contact">Contact</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#resume">Resume</a>
-                </li>
-              </ul>
-            </div>
+      <header className="header">
+        <div className="container">
+          <div className="logo">
+            <img src='../src/assets/logo.png' alt="Logo" />
           </div>
-        </nav>
+          <nav className="navbar">
+            <a className="navbar-item" href="#about">About</a>
+            <a className="navbar-item" href="#projects">Projects</a>
+            <a className="navbar-item" href="#contact">Contact</a>
+            <a className="navbar-item" href="#resume">Resume</a>
+          </nav>
+        </div>
       </header>
-
-      <main className="container py-5">
-        <About />
-        <Projects />
-        <Contact />
-        <Resume />
-      </main>
+      <About />
+      <Projects />
+      <Contact />
+      <Resume />
     </div>
   );
 }
